00ed829b4041e932c7bc1db0b13edfd700b0bb9a
Patryk Święickci <patswiecicki@gmail.com>
2021-09-16 09:06:59 +0200

    Add About to README.md and fix typo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Steve Jobs Tribute Page
+
+## About
+
+The idea for this page was taken from freeCodeCamp.
+This is a simple page about the life of Steve Jobs.
+It was mainly created using HTML and CSS.
+The website is fully responsive.
+I used JavaScript, to add two functionality.
+First one show text when the user reaches them on the page.
+Second one handles tablet's button click and turnes on/ turnes off a tablet screen.
--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,45 @@
 function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+const timelineContainers = document.querySelectorAll(
+  ".timeline-container-content"
+);
+
+const showViewedContainers = () => {
+  const containers = [...timelineContainers].filter(
+    (container) =>
+      isInViewport(container) && !container.classList.contains("show")
+  );
+  containers.forEach((container) => container.classList.add("show"));
+};
+
+document.addEventListener("scroll", showViewedContainers);
+window.onload = showViewedContainers;
+
+//iPad button
+function showHide() {
+  let imageSrc = document.querySelector(".ipad-disp img");
+
+  console.log(imageSrc.style.visibility);
+  if (window.getComputedStyle(imageSrc).visibility === "visible") {
+    imageSrc.style.opacity = 0;
+    imageSrc.style.visibility = "hidden";
+  } else {
+    imageSrc.style.opacity = 1;
+    imageSrc.style.visibility = "visible";
   }
+}
 
-  const timelineContainers = document.querySelectorAll(".timeline-container-content");
-
-  const showViewedContainers = () => {
-    const containers = [...timelineContainers].filter(
-      (container) =>
-        isInViewport(container) && !container.classList.contains("show")
-    );
-    containers.forEach((container) => container.classList.add("show"));
-  };
-
-  document.addEventListener("scroll", showViewedContainers);
-  window.onload = showViewedContainers;
-
-  //iPad button
-  function showHide() {
-    let imageSrc = document.querySelector(".ipad-disp img");
-
-    console.log(imageSrc.style.visibility);
-    if (window.getComputedStyle(imageSrc).visibility === "visible") {
-      imageSrc.style.opacity = 0;
-      imageSrc.style.visibility = "hidden";
-    }
-    else {
-      imageSrc.style.opacity = 1;
-      imageSrc.style.visibility = "visible";
-    }
-  }
-
-  /*
+//Old text showing idea.
+/*
   let timeline = document.querySelector('.timeline');
 
   timeline.addEventListener("mouseout", function( event ) {
